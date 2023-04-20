1e194de9258b4f604017b9039bb02ecd6afd183a
Ahamed Rasheed <ahamed@swivelgroup.com.au>
2023-04-21 02:56:07 +0530

    feat: integrate middleware to validate

--- src/routes/employeesRoutes.ts
+++ src/routes/employeesRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 import EmployeeController from "../controller/EmployeeController";
+import validateEmployee from "../middleware/validation";
 
 const router = Router();
 
-router.post("/", EmployeeController.createEmployee);
+router.post("/",validateEmployee, EmployeeController.createEmployee);
 router.get("/", EmployeeController.getEmployees);
 router.get("/:empId", EmployeeController.getEmployeeById);
 router.delete('/:empId', EmployeeController.deleteEmployee);
-router.patch('/:empId', EmployeeController.updateEmployeeId);
+router.patch('/:empId',validateEmployee, EmployeeController.updateEmployeeId);
 
 export default router;
