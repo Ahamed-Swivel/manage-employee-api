00f3f93e3e10f2756d7858ee2677b63c0c21ee76
Ahamed Rasheed <ahamed@swivelgroup.com.au>
2023-05-18 02:49:32 +0530

    feat: add mock test cases

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,7 @@
 import {beforeAll, describe, expect} from '@jest/globals';
 import request from "supertest";
 import app from './app';
+import { Employee } from './models/employee';
 
 // Mock the Employee model
 jest.mock("../src/models/employee");
@@ -8,6 +9,11 @@ jest.mock("../src/models/employee");
 interface IToken {
   token?: string
 }
+
+jest.fn(Employee.find).mockImplementation()
+jest.fn(Employee.findById).mockImplementation()
+jest.fn(Employee.findOneAndDelete).mockImplementation()
+jest.fn(Employee.findByIdAndUpdate).mockImplementation()
 
 describe('DELETE /api/employees/:empId', () => {
   let auth: IToken = {};
@@ -22,6 +28,10 @@ describe('DELETE /api/employees/:empId', () => {
     expect(response.text).toBe('Employee not found');
   });
 
+  it('Employee.findOneAndDelete() should called once', async () => {
+    expect(Employee.findOneAndDelete).toHaveBeenCalled();
+  });
+
   it('should return 500 if empId is invalid', async () => {
     const response = await request(app)
       .delete('/api/employees/invalid-empId')
@@ -31,7 +41,7 @@ describe('DELETE /api/employees/:empId', () => {
   });
 });
 
-describe('POST /api/employees', () => {
+describe('POST PATCH /api/employees', () => {
   let auth: IToken = {};
   beforeAll(getToken(auth));
 
@@ -68,6 +78,27 @@ describe('POST /api/employees', () => {
 
     expect(response.status).toBe(400);
   });
+
+  it('should return 404 if empId is invalid', async () => {
+    const updateEmployee = {
+      firstName: "William",
+      lastName: "Shakes",
+      email: "shakes@example.com",
+      number: "0123456789",
+      gender: "M"
+    };
+
+    const response = await request(app)
+      .patch('/api/employees/614ac8b23e09e108d78e2890')
+      .set('Authorization', `bearer ${auth.token}`)
+      .send(updateEmployee);
+
+    expect(response.status).toBe(404);
+  });
+
+  it('Employee.findOneAndDelete() should called once', async () => {
+    expect(Employee.findByIdAndUpdate).toHaveBeenCalled();
+  });
 });
 
 
@@ -75,12 +106,23 @@ describe('GET /api/employees', () => {
   let auth: IToken = {};
   beforeAll(getToken(auth));
 
-  it('should return a list of employees', async () => {
+  it('should return a 401 when authorization is not set', async () => {
+    await request(app).get('/api/employees')
+      .then(response => {
+        expect(response.status).toBe(401);
+      })
+  });
+
+  it('should return a 200 status code', async () => {
     await request(app).get('/api/employees')
       .set('Authorization', `bearer ${auth.token}`)
       .then(response => {
         expect(response.status).toBe(200);
       })
+  });
+
+  it('Employee.find() should be called once', async () => {
+    expect(Employee.find).toHaveBeenCalledTimes(1)
   });
 });
 
