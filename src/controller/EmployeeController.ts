4a2f38325253cdd58742a000aac2586993d7d3e4
Ahamed Rasheed <ahamed@swivelgroup.com.au>
2023-05-17 18:57:42 +0530

    feat: move files according to repository model

--- src/controller/EmployeeController.ts
+++ src/controller/EmployeeController.ts
@@ -1,80 +1,79 @@
 import { Request, Response } from "express";
-import { Employee } from "../models/employee";
+
 import logger from "../logger";
+import EmployeeRepository from "../repository/EmployeeRepository";
 
 class EmployeeController {
-    public async createEmployee(req: Request, res: Response): Promise<void> {
-        try {
-            const employee = new Employee(req.body);
-            await employee.save();
-            res.status(201).send(employee);
-        } catch (error) {
-            logger.error(error)
-            res.status(400).send(error);
-        }
+  public async createEmployee(req: Request, res: Response): Promise<void> {
+    try {
+      const employee = await new EmployeeRepository().createEmployee(req.body);
+      res.status(201).send(employee);
+    } catch (error) {
+      logger.error(error);
+      res.status(400).send(error);
     }
+  }
 
-    public async getEmployees(req: Request, res: Response): Promise<void> {
-        try {
-            const employees = await Employee.find();
-            res.status(200).send(employees);
-        } catch (error) {
-            logger.error(error)
-            res.status(500).send(error);
-        }
+  public async getEmployees(req: Request, res: Response): Promise<void> {
+    try {
+      const employees = await new EmployeeRepository().getEmployees();
+      res.status(200).send(employees);
+    } catch (error) {
+      logger.error(error);
+      res.status(500).send(error);
     }
+  }
 
-    public async getEmployeeById(req: Request, res: Response): Promise<void> {
-        try {
-            const { empId } = req.params
-            const employee = await Employee.findById(empId);
+  public async getEmployeeById(req: Request, res: Response): Promise<void> {
+    try {
+      const { empId } = req.params;
+      const employee = await new EmployeeRepository().getEmployeeById(empId);
 
-            if (!employee) {
-                res.status(404).send("Employee not found");
-            } else {
-                res.status(200).send(employee);
-            }
-        } catch (error) {
-            logger.error(error)
-            res.status(500).send(error);
-        }
-    }
-
-    public async deleteEmployee(req: Request, res: Response): Promise<void> {
-        try {
-            const { empId } = req.params
-            const employee = await Employee.findOneAndDelete({ _id: empId });
-
-            if (!employee) {
-                res.status(404).send("Employee not found");
-            } else {
-                res.status(200).send(employee);
-            }
-        } catch (error) {
-            logger.error(error)
-            res.status(500).send(error);
-        }
-    }
-
-    public async updateEmployeeId(req: Request, res: Response): Promise<void> {
-      try {
-          const { empId } = req.params
-          const employee = await Employee.findByIdAndUpdate(
-            empId,
-            req.body,
-            { new: true }
-          );
-
-          if (!employee) {
-            res.status(404).json({ message: "Employee not found" });
-          } else {
-            res.status(200).json(employee);
-          }
-      } catch (error) {
-          logger.error(error)
-          res.status(500).send(error);
+      if (!employee) {
+        res.status(404).send("Employee not found");
+      } else {
+        res.status(200).send(employee);
       }
+    } catch (error) {
+      logger.error(error);
+      res.status(500).send(error);
     }
+  }
+
+  public async deleteEmployee(req: Request, res: Response): Promise<void> {
+    try {
+      const { empId } = req.params;
+      const employee = await new EmployeeRepository().deleteEmployee(empId);
+
+      if (!employee) {
+        res.status(404).send("Employee not found");
+      } else {
+        res.status(200).send(employee);
+      }
+    } catch (error) {
+      logger.error(error);
+      res.status(500).send(error);
+    }
+  }
+
+  public async updateEmployeeId(req: Request, res: Response): Promise<void> {
+    try {
+      const { empId } = req.params;
+      const employee = await new EmployeeRepository().updateEmployeeById(
+        empId,
+        req.body
+      );
+
+      if (!employee) {
+        res.status(404).json({ message: "Employee not found" });
+      } else {
+        res.status(200).json(employee);
+      }
+    } catch (error) {
+      logger.error(error);
+      res.status(500).send(error);
+    }
+  }
 }
 
 export default new EmployeeController();
